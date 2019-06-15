bd1d5db5f3bbd160e837365783cd7690774ca25a
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-15 18:34:13 -0300

    Updates

---
+++ precache-manifest.641e50853367759fb04b3d8bb6e20000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a138f22cb1870782e6d6fceeb8aefda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1935ab9f61b67dd0739",
+    "url": "/static/css/main.04c4d5e7.chunk.css"
+  },
+  {
+    "revision": "4417c53e319d9beeffd5",
+    "url": "/static/js/2.a44267a1.chunk.js"
+  },
+  {
+    "revision": "e1935ab9f61b67dd0739",
+    "url": "/static/js/main.9d134a9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
