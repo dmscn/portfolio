53bf84e83b46d1f0dcf9ed3c5a8e0a4037dcc81d
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-15 00:27:09 -0300

    Installed styled-system & rebass

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import WorkInProgressPage from "./pages/WorkInProgressPage";
 
 const App: React.FC = () => {
-  return <WorkInProgressPage />;
+  return (
+    <div style={{ height: "100vh" }}>
+      <WorkInProgressPage />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/WorkInProgressPage/WorkInProgressPage.tsx
+++ src/pages/WorkInProgressPage/WorkInProgressPage.tsx
@@ -1,7 +1,12 @@
 import React, { ReactNode } from "react";
+import { Box } from "rebass";
 
 export default function WorkInProgressPage() {
-  return <Title>Work In Progress</Title>;
+  return (
+    <Box p={5} width={1} bg="black" color="white">
+      <Title>Work In Progress</Title>
+    </Box>
+  );
 }
 
 const Title = (props: { children: ReactNode }) => <h1>{props.children}</h1>;
