526350d680922d7ee82e667bab4cf4367f18e190
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-15 11:23:31 -0300

    Updated eslintrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import WorkInProgressPage from "./pages/WorkInProgressPage";
+import React from 'react'
+import WorkInProgressPage from './pages/WorkInProgressPage'
 
 const App: React.FC = () => {
   return (
-    <div style={{ height: "100vh" }}>
+    <div style={{ height: '100vh' }}>
       <WorkInProgressPage />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/pages/WorkInProgressPage/WorkInProgressPage.tsx
+++ src/pages/WorkInProgressPage/WorkInProgressPage.tsx
@@ -1,12 +1,12 @@
-import React, { ReactNode } from "react";
-import { Box } from "rebass";
+import React, { ReactNode } from 'react'
+import { Box } from 'rebass'
 
 export default function WorkInProgressPage() {
   return (
     <Box p={5} width={1} bg="black" color="white">
       <Title>Work In Progress</Title>
     </Box>
-  );
+  )
 }
 
-const Title = (props: { children: ReactNode }) => <h1>{props.children}</h1>;
+const Title = (props: { children: ReactNode }) => <h1>{props.children}</h1>
