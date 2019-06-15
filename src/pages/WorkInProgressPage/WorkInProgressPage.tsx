c70fd211f090d2eabb20c62da71799142a8eb150
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-15 19:58:05 -0300

    Incresed reset time to 2000ms

--- src/pages/WorkInProgressPage/WorkInProgressPage.tsx
+++ src/pages/WorkInProgressPage/WorkInProgressPage.tsx
@@ -9,7 +9,7 @@ export default function WorkInProgressPage() {
     <Box center full>
       <Typing loop>
         <h1>Work in Progress...</h1>
-        <Typing.Reset count={1} delay={1500} />
+        <Typing.Reset count={1} delay={2000} />
       </Typing>
     </Box>
   )
