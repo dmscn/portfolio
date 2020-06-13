4190b1b83fc283351ff02e560a1349de85b5ed2a
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-06-13 20:33:42 -0300

    Updates

---
+++ precache-manifest.3511508ba001e3200e2332b7a2eb6d25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3a1975d70a4d5735359744ca318e86",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f17d614e680e5b05cd77",
+    "url": "/portfolio/static/css/main.04c4d5e7.chunk.css"
+  },
+  {
+    "revision": "93ab3146824867c00eac",
+    "url": "/portfolio/static/js/2.81b48016.chunk.js"
+  },
+  {
+    "revision": "f17d614e680e5b05cd77",
+    "url": "/portfolio/static/js/main.0fbd00cd.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
